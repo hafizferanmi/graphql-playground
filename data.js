553b20332b9ee37aa538d6b19dd52fe2b0e9c35a
Hafizferanmi <hafizferanmi@gmail.com>
2021-07-25 06:01:39 +0100

    Resolved type LIBRARY/BOOKS/QUERY

---
+++ data.js
@@ -0,0 +1,55 @@
+const books = [
+  {
+    id: "book1",
+    title: "The Awakening",
+    library: "library1",
+    author: "author1",
+  },
+  {
+    id: "book2",
+    title: "City of Glass",
+    library: "library2",
+    author: "author1",
+  },
+  {
+    id: "book3",
+    title: "The running Lion",
+    library: "library1",
+    author: "author1",
+  },
+];
+
+const libraries = [
+  {
+    id: "library1",
+    name: "Ezekiah Oluwasanmi",
+  },
+  {
+    id: "library2",
+    name: "Bola Tinubu library",
+  },
+];
+
+const authors = [
+  {
+    id: "author1",
+    name: "Boda Afisi",
+  },
+  {
+    id: "author2",
+    name: "Tade Dada",
+  },
+];
+
+const addresses = {
+  library1: "Ajanlekoko street and some other addressess",
+  library2: "Mokola, Ibadan",
+  library3: "Iwo road",
+};
+
+module.exports = {
+  books,
+  libraries,
+  authors,
+  addresses,
+};
---
+++ index.js
@@ -0,0 +1,58 @@
+const { gql, ApolloServer } = require("apollo-server");
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
+const { books, libraries, authors, addresses } = require("./data");
+
+const typeDefs = gql`
+  type Author {
+    name: String
+  }
+  type Book {
+    title: String
+    author: Author
+    library: Library
+  }
+  type Library {
+    name: String
+    books: [Book]
+    address: String
+  }
+  type Query {
+    greeting: String
+    libraries: [Library]
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greeting: () => "Welcome to my graphql playground",
+    libraries: (parent, args, context, info) => {
+      return libraries;
+    },
+    books: () => books,
+  },
+  Book: {
+    library: (book, args, context, info) => {
+      return libraries.find((library) => book.library == library.id);
+    },
+    author: (book) => authors.find((author) => author.id == book.author),
+  },
+  Library: {
+    address: (library, args, context, info) => {
+      return addresses[library.id];
+    },
+    books: (library, args, context, info) => {
+      return books.filter((book) => book.library === library.id);
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground({})],
+});
+
+server.listen(4200).then(() => console.log("App running!!"));
