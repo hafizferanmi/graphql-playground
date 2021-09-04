b3a2500ab34997ccd0211496f3ea700eda12d8af
hafizferanmi <hafizferanmi@gmail.com>
2021-09-04 16:36:13 +0100

    Package: graphql-tools

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const { ApolloServer } = require("apollo-server");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 const {
   ApolloServerPluginLandingPageGraphQLPlayground,
 } = require("apollo-server-core");
 const typeDefs = require("./graphql/typedef");
 const resolvers = require("./graphql/resolvers");
 
-const server = new ApolloServer({
+const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground({})],
+});
+
+const plugins = [ApolloServerPluginLandingPageGraphQLPlayground({})];
+
+const server = new ApolloServer({
+  schema,
+  plugins,
 });
 
 server.listen(4200).then(() => console.log("App running!!"));
